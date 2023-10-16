50a508111707db92464b83613a675af462c8cdfc
caesar003 <caesarmuksid@gmail.com>
2023-10-16 16:50:20 +0700

    feat: fetching to homepage

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Models\Course;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+
+        return view('welcome', ['courses' => Course::all()]);
+    }
+}
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,4 +10,10 @@ class Course extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'user_id', 'is_published', 'price', 'thumbnail'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
