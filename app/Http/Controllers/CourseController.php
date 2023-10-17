e3bc5aeb308b0a54915d68d8062f6a5367e276fa
caesar003 <caesarmuksid@gmail.com>
2023-10-17 12:57:01 +0700

    fix: fix missing count

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Models\Course;
 use \App\Models\Category;
+use Illuminate\Support\Facades\DB;
 
 class CourseController extends Controller
 {
@@ -22,7 +23,7 @@ class CourseController extends Controller
         }
 
 
-        $query = Course::select('courses.id', 'courses.title', 'courses.thumbnail', 'courses.price', 'courses.category_id', 'categories.name as category_name')
+        $query = Course::select('courses.id', 'courses.title', 'courses.thumbnail', 'courses.price', 'courses.category_id', DB::raw('COUNT(chapters.course_id) as chapters_count'), 'categories.name as category_name')
             ->leftJoin('chapters', 'courses.id', '=', 'chapters.course_id')
             ->leftJoin('categories', 'courses.category_id', '=', 'categories.id')
             ->groupBy('courses.id', 'courses.title', 'courses.thumbnail', 'courses.price', 'courses.category_id', 'categories.name');
