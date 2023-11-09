7a8dbe48f31f30cf40dbe9b42f4f92647e59e5b4
ahmadrosid <alahmadrosid@gmail.com>
2023-11-10 04:38:22 +0700

    fix: vite build

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
                 "resources/js/app.js",
+                "resources/css/app.css",
                 "public/css/choices.min.css",
+                "public/css/fastbootstrap.min.css",
             ],
             refresh: true,
         }),
